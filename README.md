e971073461cdd4263c437d7b25f94ddff55a7119
Bradford Ware <bware8@gatech.edu>
2016-11-18 11:40:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# FoodFinder
+Web application tool to find your next meal.
+
+For more technical documentation check out `FoodFinder.pdf`
+
+## Technologies
+* [Node.js](//nodejs.org/en)
+* [Express](//expressjs.com)
+* [Node-Yelp ](//github.com/olalonde/node-yelp)
+* [Bower](//https://bower.io)
+* [Bootstrap](//getbootstrap.com/)
+* [Font Awesome](//fontawesome.io)
+* [jQuery 2.x](//code.jquery.com)
+* [Yelp Search API](//www.yelp.com/developers/documentation/v2/search_api)
+
+## Getting Started
+* download [Node.js](//nodejs.org/en/download/) or `brew install node`
+* download and decompress `food-finder.zip`
+* `cd food-finder`
+* `node server.js`
+* visit `localhost:8000` in your browser
---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Dependencies from NPM
+var express = require('express');
+var yelp = require('yelp');
+var app = express();
+
+// Constants
+var port = 8000;
+var consumer_key = 'dMy7osovALGZw-wy-P3P7g';
+var consumer_secret = 'XSBYeVIvXv7gtAvHGqH9W08W-0w';
+var token = 'qCX0ddXOnXGngJHcJpT16QyD6WFDqOpj';
+var token_secret = 'ETPHr-bOb3dU6twK3fLhOGnf5QI';
+
+// Makes static assets available and starts server listening on port
+app.use(express.static('public'));
+app.listen(port);
+
+// Initializes Yelp API connection
+var yelpApi = new yelp({
+  consumer_key: consumer_key,
+  consumer_secret: consumer_secret,
+  token: token,
+  token_secret: token_secret,
+});
+
+/**
+* Back end API routing using Express
+* Only listens for GET requests from client
+* Takes query from client and passes request to Yelp API
+* Returns response from Yelp API to client
+*/
+app.get('/search', function (req, res) {
+  yelpApi.search(req.query)
+	.then(function(data) {
+	  res.send(data);
+	})
+	.catch(function(err) {
+    res.status(err.statusCode).send(err.data);
+	});
+});
